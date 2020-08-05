e1a6e20600f6482f598b39284ff25c28b9b461eb
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-08-05 15:12:38 -0500

    added two apis

--- index.js
+++ index.js
@@ -1,60 +1,70 @@
 // const axios = require("axios");
 // require("dotenv").config()
 // const apiKey = process.env.API_KEY
-const apiKey = "1edb01805b03ec766525435c8a6effd0";
+// const apiKey = "1edb01805b03ec766525435c8a6effd0";
 // Build an app that allows a user to find breweries near them.
 
 let arrayOfBeers;
+let arrayOfLows;
+// let arrayOfHighs;
 
+// eslint-disable-next-line no-undef
 window.onload = function () {
   getBeers();
+  low();
 };
 
 const getBeers = () => {
-  const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  const url =
-    "https://sandbox-api.brewerydb.com/v2/beer/random?key=1edb01805b03ec766525435c8a6effd0";
-
-  fetch(proxyurl + url)
+  fetch("https://api.punkapi.com/v2/beers")
     .then((res) => res.json())
-    .then((response) => (arrayOfBeers = response))
-    .catch(() =>
-      console.log("Can’t access " + url + " response. Blocked by browser?")
-    );
+    .then((response) => (arrayOfBeers = response));
+  //     .catch((error) => console.log(error));
 };
 
 const consoleBeer = () => {
-  console.log(arrayOfBeers);
+  console.log(arrayOfLows);
 };
 
-// const getBeers = () => {
-//   axios(
-//     "https://sandbox-api.brewerydb.com/v2/beer/random?key=1edb01805b03ec766525435c8a6effd0",
-//     {
-//       method: "HEAD",
-//       mode: "no-cors",
-//       headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//       },
-//       withCredentials: true,
-//       credentials: "same-origin",
-//       crossdomain: true,
-//     }
-//   )
-//     .then((response) => (arrayOfBeers = response))
-//     .catch((error) => console.log(error));
+const low = () => {
+  fetch("https://api.punkapi.com/v2/beers?abv_lt=5")
+    .then((res) => res.json())
+    .then((response) => (arrayOfLows = response));
+};
+
+// const high = () => {
+//   fetch(" https://api.punkapi.com/v2/beers?abv_gt=5")
+//     .then((res) => res.json())
+//     .then((response) => (arrayOfHighs = response));
 // };
 
-// const displayBeers = () => {
-//   const allBeer = document.getElementById("all-beer")
-//   arrayOfBeers.forEach(beer) => {
-//     const li = document.createElement("li")
-//     const randomBeer = document.createTextNode(
-//       `Beer Name: ${name}, Beer Style: ${style.name}, ABV: ${abv}, Description: ${style.description}`)
-//     }
-//     li.appendChild(randomBeer)
-//         allBeer.append(li)
-//         console.log(beer)
-// }
+const displayBeers = () => {
+  const allBeer = document.getElementById("all-beer");
+  arrayOfBeers.forEach((beer) => {
+    const li = document.createElement("li");
+    const beerObject = document.createTextNode(
+      `Beer Name: ${beer.name}, Tagline: ${beer.tagline}, ABV: ${beer.abv}, Description: ${beer.description}`
+    );
+    li.appendChild(beerObject);
+    allBeer.append(li);
+    console.log(beer);
+  });
+};
+
+const lowBeers = () => {
+  const lowBeer = document.getElementById("all-beer");
+  arrayOfLows.forEach((beer) => {
+    const li = document.createElement("li");
+    const beerObject = document.createTextNode(
+      `Beer Name: ${beer.name}, Tagline: ${beer.tagline}, ABV: ${beer.abv}, Description: ${beer.description}`
+    );
+    li.appendChild(beerObject);
+    lowBeer.append(li);
+    console.log(beer);
+  });
+};
+
+//          <button onclick="consoleBeer()">Console Beer</button>
+//         <button onclick="lowBeer()">Low ABV</button>
+//         <button onclick="midBeer()">Mid ABV</button>
+//         <button onclick="highBeer()">High ABV</button>
+//         <button onclick="randomBeer()">RANDOM</button>
