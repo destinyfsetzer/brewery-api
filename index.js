b4d9f35ab7250961cf1b80884b30dfa6564711ac
ArielHolman <ArielARodrigues01@gmail.com>
2020-08-05 19:24:10 -0500

    added tmeplate literals, updated low beers with some style

--- index.js
+++ index.js
@@ -66,12 +66,12 @@ const lowBeers = () => {
   clearList();
   const allBeer = this.document.getElementById("all-beer");
   arrayOfLows.forEach((beer) => {
-    const li = this.document.createElement("li");
-    const beerObject = this.document.createTextNode(
-      `Beer Name: ${beer.name}, Tagline: ${beer.tagline}, ABV: ${beer.abv}, Description: ${beer.description}`
-    );
-    li.appendChild(beerObject);
-    allBeer.append(li);
+    const beerInfo = `<li class="beer-item">
+    <h4 class="title">${beer.name} - <span class="beer-abv">${beer.abv} abv</span></h4>
+    <h6>${beer.tagline}</h6>
+    <h6>${beer.description}</h6>
+    </li>`;
+    allBeer.insertAdjacentHTML("afterbegin", beerInfo);
     console.log(beer);
   });
 };
